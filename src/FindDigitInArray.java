6de4111ffb7270468e3402754afbd70ef06f897d
Anton P. <made1of1fire@gmail.com>
2024-04-13 19:25:42 +0300

    fix(FindDigitInArray): fixed general metod for binarySearch() -> now it binarySearchLeft() it finds the first occurrence of the given number K

--- src/FindDigitInArray.java
+++ src/FindDigitInArray.java
@@ -9,35 +9,35 @@ import java.util.Arrays;
  * */
 public class FindDigitInArray {
     public static void main(String[] args) {
-        int[] n = {0, 0, 2, 4, 5, 8, 8, 9, 11, 15, 17, 22};
+        int[] n = {0, 0, 2, 4, 5, 8, 8, 8, 9, 11, 15, 17, 22};
         int[] k = {8, 7, 22, 0, 23, -1};
         int[] empty = {};
 
         System.out.println("Array - " + Arrays.toString(n));
         for (int i = 0; i < k.length; i++) {
             System.out.println("\tDigit [" + k[i] + "] in array? - "
-                    + binarySearch(n, k[i]));
+                    + binarySearchLeft(n, k[i]));
         }
     }
 
-    private static boolean binarySearch(int[] array, int target) {
+    private static boolean binarySearchLeft(int[] array, int target) {
         if (array == null || array.length < 1) {
             return false;
         }
 
         int min = 0;
         int max = array.length - 1;
+        int cursor, targetIndex = -1;
 
-        do {
-            int cursor = (min + max + 1) / 2;
-            if (array[cursor] == target) {
-                return true;
-            } else if (array[cursor] > target) {
+        while (min <= max) {
+            cursor = min + (max - min) / 2;
+            if (array[cursor] >= target) {
+                targetIndex = cursor;
                 max = cursor - 1;
-            } else {
-                min = cursor;
+            } else if (array[cursor] < target) {
+                min = cursor + 1;
             }
-        } while (min < max);
-        return array[min] == target;
+        }
+        return targetIndex != -1 && array[targetIndex] == target;
     }
 }
